55ac4f35bff55629a0017ea84554e9c8fe467b71
Camilla Ohrt Lerche <cal001@edu.zealand.dk>
2023-11-29 10:56:02 +0100

    Update

---
+++ Henry/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Henry.Models
+{
+    public class Member
+    {
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "Udfyld dit navn"), DisplayName("Navn")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Angiv telefonnummer"), DisplayName("Telefonnummer")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Udfyld mailaddresse"), DisplayName("Mailaddresse")]
+        public string Email { get; set; }
+        [DisplayName("Kodeord")]
+        public string Password { get; set; }
+        private bool Memberstatus { get; set; }
+    }
+}
---
+++ Henry/Services/MemberRepository.cs
@@ -0,0 +1,30 @@
+﻿using Henry.Helpers;
+using Henry.Interfaces;
+using Henry.Models;
+
+namespace Henry.Services
+{
+    public class MemberRepository:IMemberRepository
+    {
+        private string JsonFileName = @"Data\JsonMember.json";
+
+        public List<Member> GetAllMembers()
+        {
+            return JsonFileReader<Member>.ReadJson(JsonFileName);
+        }
+
+ 
+        
+        public Member GetMembers(int id)
+        {
+            List<Member> members = JsonFileReader<Member>.ReadJson(JsonFileName);
+            foreach (var item in members)
+            {
+                if (item.UserId == id)
+                    return item;
+            }
+            return null;
+        }
+
+    }
+}

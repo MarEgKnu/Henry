7bca9b4cb0e86c965f55d35e7fd2917b17f8210b
Marius Egede Knudsen <mak004@edu.zealand.dk>
2023-11-30 09:35:40 +0100

    stufgf

---
+++ Henry/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿namespace Henry.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Img { get; set; }
+
+        public int CreatorUserId { get; set; }
+
+        public DateTime Created {  get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}

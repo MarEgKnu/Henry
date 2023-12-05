2a76c1822ade572148ff72cec2d8e774837d8fd6
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2023-12-05 12:53:54 +0100

    Update photo

--- Henry/Pages/Blogs/EditBlog.cshtml.cs
+++ Henry/Pages/Blogs/EditBlog.cshtml.cs
@@ -1,5 +1,7 @@
+using Henry.Helpers;
 using Henry.Interfaces;
 using Henry.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,22 +10,44 @@ namespace Henry.Pages.Blogs
     public class EditBlogModel : PageModel
     {
         private IBlogRepository _blogRepo;
+        private IWebHostEnvironment _webHostEnvironment;
         private Blog _blogToUpdate;
+        [BindProperty]
+        public IFormFile? Photo { get; set; }
 
         [BindProperty]
         public Blog BlogToUpdate { get { return _blogToUpdate; } set { _blogToUpdate = value; } }
 
-        public EditBlogModel(IBlogRepository blogRepository)
+        public EditBlogModel(IBlogRepository blogRepository, IWebHostEnvironment webHostEnvironment)
         {
             _blogRepo = blogRepository;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public void OnGet(int id)
         {
             BlogToUpdate = _blogRepo.GetBlog(id);
         }
-        public IActionResult OnPostUpdate()
+        public IActionResult OnPost()
         {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (Photo != null)
+            {
+                if (_blogToUpdate.Img != null)
+                {
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "/Imgs/EventImages", _blogToUpdate.Img);
+                    System.IO.File.Delete(filePath);
+                }
+
+                _blogToUpdate.Img = FileHelpers.ProcessUploadedFile("Imgs/blogImages", Photo, _webHostEnvironment);
+            }
+            else
+            {
+                _blogToUpdate.Img = _blogRepo.GetBlog(_blogToUpdate.Id).Img;
+            }
             BlogToUpdate.LastUpdated = DateTime.Now;
             _blogRepo.UpdateBlog(BlogToUpdate);
             return RedirectToPage("Index");

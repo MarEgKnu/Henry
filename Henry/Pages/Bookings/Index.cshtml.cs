8b5aad22854fa2a33c9405e6eb893d1e62a4c941
Marius Egede Knudsen <mak004@edu.zealand.dk>
2023-12-08 10:32:41 +0100

    empty index page for bookings

---
+++ Henry/Pages/Bookings/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Henry.Pages.Bookings
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

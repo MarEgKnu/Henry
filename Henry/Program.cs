244bdb3cd6dbec1240b73c1cdd4d988eda1064b8
Camilla Ohrt Lerche <cal001@edu.zealand.dk>
2023-12-13 09:55:18 +0100

    Members again

--- Henry/Program.cs
+++ Henry/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddTransient<IEventRepository, EventRepository>();
-
+builder.Services.AddTransient<IMemberRepository, MemberRepository>();
 builder.Services.AddTransient<IBoatRepository, BoatRepository>();   
 
 var app = builder.Build();
--- Henry/Services/MemberRepository.cs
+++ Henry/Services/MemberRepository.cs
@@ -7,15 +7,52 @@ namespace Henry.Services
     public class MemberRepository:IMemberRepository
     {
         private string JsonFileName = @"Data\JsonMember.json";
-
+        public void CreateMember(Member me)
+        {
+            List<Member> members = GetAllMembers();
+            bool addId;
+            for (int i = 1; i <= members.Count + 1; i++)
+            {
+                addId = true;
+                foreach (var item in members)
+                {
+                    if (i == item.UserId)
+                    {
+                        addId = false;
+                    }
+                }
+                if (addId)
+                {
+                    me.UserId = i;
+                    break;
+                }
+            }
+            members.Add(me);
+            JsonFileWriter<Member>.WriteToJson(members, JsonFileName);
+        }
         public List<Member> GetAllMembers()
         {
             return JsonFileReader<Member>.ReadJson(JsonFileName);
         }
 
- 
-        
-        public Member GetMembers(int id)
+        public void DeleteMember(Member me)
+        {
+            List<Member> members = GetAllMembers();
+            foreach (Member m in members)
+            {
+                if (m.UserId == me.UserId)
+                {
+                    members.Remove(m);
+                    JsonFileWriter<Member>.WriteToJson(members, JsonFileName);
+                    break;
+
+                }
+            }
+   
+
+        }
+
+        public Member GetMember(int id)
         {
             List<Member> members = JsonFileReader<Member>.ReadJson(JsonFileName);
             foreach (var item in members)
@@ -25,6 +62,24 @@ namespace Henry.Services
             }
             return null;
         }
-
+        public void UpdateMember(Member me)
+        {
+            if (me != null)
+            {
+                List<Member> members = GetAllMembers();
+                foreach (Member m in members)
+                {
+                    if (me.UserId == m.UserId)
+                    {
+                        m.Name = me.Name;
+                        m.Phone = me.Phone;
+                        m.Email = me.Email;
+                        m.Pb = me.Pb;
+                        break;
+                    }
+                }
+                JsonFileWriter<Member>.WriteToJson(members, JsonFileName);
+            }
+        }
     }
 }
